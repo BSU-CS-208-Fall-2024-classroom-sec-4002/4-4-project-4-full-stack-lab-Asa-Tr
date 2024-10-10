dcd6452907141c8da27b710542f9d681d5111a0a
Asa-Tr <asatribble@u.boisestate.edu>
2024-10-10 16:22:34 -0600

    Project Complete

--- Retrospective.md
+++ Retrospective.md
@@ -1,16 +1,21 @@
 # Retrospective
 
-- name: Jane Doe
-- email: janedoe@u.boisestate.edu
+- name: Asa Tribble
+- email: asatribble@u.boisestate.edu
 
 ## Experience
 
-TODO
+Overall, the experience was just fine. I was able to get everything working with minimal problems. My live preview wouldn't work with this system so I had to just get the localhost through the browser. I decided to have a bit of fun with the extra-credit opportunities in this project too. I did basic sanitization for all of the inputs to any SQL statements and it works fine so it can filter out stuff like comment injection, plain incorrect inputs, etc. I also took the liberty of adding a 'clear list' input so that the whole list can be cleared at once instead of getting arthritis by clearing all the elements individually. Lastly, I also tried to go for the ugliest site extra-credit. That I think went well but it hurts if you look at it for too long. I have it turned off by default because it is so bad, and you can just uncomment the specified lines to get it to work. I do certainly think it meets the definition of ugly though, since I can’t stand to look at it. All I did was reuse all of the assets found in this project and did not bring in anything new.
 
 ## Known issues or Bugs
 
-TODO
+ - The CSS styling is abhorrent, but, it meets specifications
 
 ## Sources used
 
-TODO
+Rotation animation for CSS - https://stackoverflow.com/questions/16771225/css3-rotate-animation
+<br>
+Checking if a table exists - https://stackoverflow.com/questions/59514987/how-to-check-if-a-table-exists-in-sqlite3-nodejs
+<br>
+
+
--- src/server.js
+++ src/server.js
@@ -6,9 +6,8 @@ const sqlite3 = sql.verbose()
 // Create an in memory table to use
 const db = new sqlite3.Database(':memory:')
 
-// This is just for testing you would not want to create the table every
-// time you start up the app feel free to improve this code :)
-db.run(`CREATE TABLE todo (
+//I made this only create the table if it doesn't exist already
+db.run(`CREATE TABLE IF NOT EXISTS todo (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     task TEXT NOT NULL)`)
 
@@ -18,26 +17,58 @@ app.set('views', 'views')
 app.set('view engine', 'pug')
 app.use(express.urlencoded({ extended: false }))
 
+//Base page that loads the tasks object to the pug file
 app.get('/', function (req, res) {
-    //TODO You will need to do a SQL select here
-    //TODO You will need to update the code below!
+    const local = { tasks: [] }
+    db.each('SELECT id, task FROM todo', function (err, row) {
+        if (err) {
+            console.log(err)
+        } else {
+            local.tasks.push({ id: row.id, task: row.task })
+        }
+    }, function (err, count) {
+        if (!err) {
+            res.render('index', local)
+        } else {
+            console.log(err)
+        }
+    })
     console.log('GET called')
-    res.render('index')
-
 })
-
+//path that adds a new element to the database if it meets requirements
 app.post('/', function (req, res) {
     console.log('adding todo item')
-    //TODO You will need to to do a SQL Insert here
-
+    //basic input checks for a SQL non-null text sequence
+    if(req.body.todo != null && req.body.todo.length > 0 && req.body.todo.substring(0,2)!='--'){
+        const stmt = db.prepare('INSERT INTO todo (task) VALUES (?)')
+        stmt.run(req.body.todo)
+        stmt.finalize()
+    }else{
+        console.log(`Failed to add the element ${req.body.todo} to the database since it contains an improper format.`)
+    }
+    res.redirect('/')
 })
-
+//path that removes an element from the database if it meets requirements
 app.post('/delete', function (req, res) {
     console.log('deleting todo item')
-    //TODO you will need to delete here
-
+    if(req.body.id != null && typeof parseInt(req.body.id) == Number){
+        const stmt = db.prepare('DELETE FROM todo where id = (?)')
+        stmt.run(req.body.id)
+        stmt.finalize()
+    }else{
+        console.log(`Failed to remove the element with id ${req.body.id} from the database since it contains an improper format.`)
+    }
+    res.redirect('/')
 })
-
+//path that clears the whole table and starts anew
+app.post('/clear', function (req, res) {
+    console.log('clearing todo list')
+    db.run('DROP TABLE todo')
+    db.run(`CREATE TABLE IF NOT EXISTS todo (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        task TEXT NOT NULL)`)
+    res.redirect('/')
+})
 // Start the web server
 app.listen(3000, function () {
     console.log('Listening on port 3000...')
